6d62861a8d77b7e29cab1186b53bfb8d6ba88b1b
gydisme <kingterrygyd@gmail.com>
2016-07-27 12:18:01 +0800

    Make the Csv Parser could parse a single line

--- Assets/DataManager/Custom/TableParserCsv.cs
+++ Assets/DataManager/Custom/TableParserCsv.cs
@@ -26,33 +26,37 @@ public class TableParserCsv : TableParser
 	{
 		List<List<string>> result = new List<List<string>>();
 		List<string> lines;
-
 		string line = string.Empty;
-		bool inQuato = false;
-		bool addQuato = false;
-		string value = string.Empty;
 
 		while( null != ( line = readFile.ReadLine() ) )
 		{
-			lines = new List<string>();
-			value = string.Empty;
-
-			foreach( char c in line )
-			{
-				if( addQuato && c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { addQuato = false; }
-				else if( addQuato && !c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { addQuato = false; inQuato = false; }
-				else if( inQuato && c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { addQuato = true; continue; }
-				else if( c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { inQuato = true; continue; }
-
-				if( !inQuato && c.Equals( EDataManager.COMMA_CHAR ) ) { lines.Add( value ); value = string.Empty; continue; }
-
-				value += c.ToString();
-			}
-
-			lines.Add( value );
+			lines = ParseLine( line );
 			result.Add( lines );
 		}
 
+		return result;
+	}
+
+	public static List<string> ParseLine( string line )
+	{
+		List<string> result = new List<string>();
+		string value = string.Empty;
+		bool inQuato = false;
+		bool addQuato = false;
+
+		foreach( char c in line )
+		{
+			if( addQuato && c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { addQuato = false; }
+			else if( addQuato && !c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { addQuato = false; inQuato = false; }
+			else if( inQuato && c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { addQuato = true; continue; }
+			else if( c.Equals( EDataManager.DOUBLE_QUOTATION_MARKS_CHAR ) ) { inQuato = true; continue; }
+
+			if( !inQuato && c.Equals( EDataManager.COMMA_CHAR ) ) { result.Add( value ); value = string.Empty; continue; }
+
+			value += c.ToString();
+		}
+
+		result.Add( value );
 		return result;
 	}
 }
